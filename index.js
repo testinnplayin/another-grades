6dc0116feb97cba8d3b3a78448b8ba4387aa8237
R Wood <archaurore@gmail.com>
2019-04-01 11:08:44 +0200

    set up connection material

--- index.js
+++ index.js
@@ -12,10 +12,30 @@ mongoose.Promise = global.Promise;
 const PORT = process.env.PORT;
 const DATABASE = process.env.DATABASE;
 
-console.log(PORT, DATABASE);
-
 mongoose.set('useFindAndModify', false);
 
-http.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+mongoose.connect(DATABASE, { useNewUrlParser : true })
+    .then(() => {
+        console.log('Connected to database');
+        http.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch(err => console.error('Error while trying to connect to database ', err));
+
+
+mongoose.connection.on('disconnect', () => {
+    console.warn('Database connection disconnecting');
+});
+
+http.on('close', () => {
+    console.warn('Server closed');
+});
+
+process.on('SIGINT', () => {
+    console.warn('Server being purposely shut down');
+    mongoose.connection.close(() => {
+        console.warn('Database shutting down');
+        process.exit(1);
+    });
 });
