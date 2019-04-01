5658244b5dfee6233018176c3695e72ce22ab0c4
R Wood <archaurore@gmail.com>
2019-04-01 11:20:42 +0200

    added a catch-all route

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 
+
+// Catch-all route
+
+app.use('*', (req, res) => {
+    return res.status(404).json({ message : 'Resource not found' });
+});
+
 module.exports = app;
