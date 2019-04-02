474b21007b28612a9a38d5c227c8e2276e763b55
R Wood <archaurore@gmail.com>
2019-04-02 13:21:09 +0200

    made a constants folder with commonly-used error messages

---
+++ constants/error-msgs.js
@@ -0,0 +1,18 @@
+/**
+ * @module constants/error-msgs
+ * Module containing commonly-used error messages
+ * @author R.Wood
+ * Date: 02/04/2019
+ */
+
+'use strict';
+
+module.exports = {
+    /**
+     * Function that returns a message for a badly-formed request(400 error)
+     * @returns {string} The message
+     */
+    getBadlyFormedReq () {
+        return 'Badly-formed request';
+    }
+};
