b60c8cd162b6822f83de7ec2392f2768d63accf2
R Wood <archaurore@gmail.com>
2019-04-02 17:13:56 +0200

    added comments

--- models/klass-history.js
+++ models/klass-history.js
@@ -1,40 +1,64 @@
+/**
+ * @module model/klass-history
+ * Module that creates a KlassHistory model
+ * @author R.Wood
+ * Date: 02/04/2019
+ */
+
 'use strict';
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const klassHistorySchema = new Schema({
-    class_id : {
-        type : Schema.Types.ObjectId,
-        ref : 'Klass',
-        required : true,
-        index : true
+/**
+ * Create a new class history schema in Mongoose; this model is much like an instance of a class
+ * @class
+ */
+const klassHistorySchema = new Schema(
+    /** @lends klassHistorySchema.prototype */
+    {
+        /** @member {string} class_id - ObjectId of associated class document */
+        class_id : {
+            type : Schema.Types.ObjectId,
+            ref : 'Klass',
+            required : true,
+            index : true
+        },
+        /** @member {number} year - the year the class is being given */
+        year : {
+            type : Number,
+            required : true
+        },
+        /** @member {string} [semester=N/A] - the semester the class is being given */
+        semester : {
+            type : String,
+            /** @enum {string} */
+            enum : [
+                'FALL',
+                'SPRING',
+                'SUMMER',
+                'N/A'
+            ],
+            default : 'N/A'
+        },
+        /** @member {string[]} [students] - an array of student ObjectIds */
+        students : [{
+            type : Schema.Types.ObjectId,
+            ref : 'Student'
+        }]
     },
-    year : {
-        type : Number,
-        required : true
-    },
-    semester : {
-        type : String,
-        enum : [
-            'FALL',
-            'SPRING',
-            'SUMMER',
-            'N/A'
-        ],
-        default : 'N/A'
-    },
-    students : [{
-        type : Schema.Types.ObjectId,
-        ref : 'Student'
-    }]
-},
-{
-    collection : 'class-history'
-});
+    {
+        /** @property {string} collection - The name of the collection, which is class-history */
+        collection : 'class-history'
+    });
 
 // METHODS
 
+/**
+ * @function
+ * @param {Object} klass - a class document
+ * @returns a KlassHistory document for display by the client
+ */
 klassHistorySchema.methods.showKlass = function(klass) {
     return {
         class_id : this.class_id,
@@ -51,4 +75,8 @@ klassHistorySchema.methods.showKlass = function(klass) {
 
 const KlassHistory = mongoose.model('KlassHistory', klassHistorySchema);
 
+/**
+ * Exports the KlassHistory model
+ * @type {Object}
+ */
 module.exports = KlassHistory;
