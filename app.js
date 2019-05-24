ab67318889ba4771303c21371db798406afafe8c
R Wood <archaurore@gmail.com>
2019-05-25 00:02:34 +0200

    began work on basic styles and on nav

--- app.js
+++ app.js
@@ -7,6 +7,8 @@
 
 'use strict';
 
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
@@ -14,18 +16,23 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+/** Set PUG as the template engine */
+app.set('view engine', 'pug');
+
 /** 
- * @see module:routes/klass-router */
+ * @see module:routes/klass-router
+ * @see module:routes/klass-history-router
+ */
 
 const klassRouter = require('./routes/klass-router');
-
-/**
- * @see module:routes/klass-history-router */
- 
 const klassHistoryRouter = require('./routes/klass-history-router');
+const viewRouter = require('./routes/view-router');
 
 app.use('/api/classes', klassRouter);
 app.use('/api/class-histories', klassHistoryRouter);
+app.use('/', viewRouter);
 
 // Catch-all route
 
