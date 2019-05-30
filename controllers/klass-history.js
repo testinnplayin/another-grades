e9928e1c29873b4e2109262bc87b9134d98116e7
R Wood <archaurore@gmail.com>
2019-05-30 13:28:38 +0200

    separated out some logic

--- controllers/klass-history.js
+++ controllers/klass-history.js
@@ -1,95 +1,26 @@
 /**
- * @module controllers/klass-history-controllers
+ * @module KlassHistoryController
  * Module that contains the KlassHistory controller
  * @author R.Wood
  * Date: 02/04/2019
- * @requires module:models/klass
  * @requires module:models/klass-history
- * @requires module:helpers/checkers.checkReqFields
- * @requires module:constants/error-messages
+ * @requires module:checkers.checkReqFields
+ * @requires module:klassHistoryConstants.error-messages
  * @requires module:verification/klass
+ * @requires module:verification/klassHistory
  */
 
-/** @see module:models.klass*/
-const Klass = require('../models/klass');
-
-/** @see module:models.klass-history */
+/** @see module:models/klass-history */
  const KlassHistory = require('../models/klass-history');
 
-/** @see module:helpers.checkers.checkReqFields */
+/** @see module:checkers.checkReqFields */
 const { checkReqFields } = require('../helpers/checkers');
 
-/** @see module:constants.errorMsgs.getBadlyFormedReq */
-const { getBadlyFormedReq } = require('../constants/error-msgs');
+/** @see module:klassHistoryConstants.errMsgs */
+const errMsgs = require('../constants/klass-history-msgs');
 
-/** @see module:verification.klass */
-const klassVerifiers = require('../verification/klass');
-
-/** @constant {Object} errMsgs - contains error messages used multiple times in this controller */
-const errMsgs = {
-    badReqMsg : getBadlyFormedReq(),
-    noClass : 'cannot find associated class',
-    noKH : 'cannot find class history',
-    invSemester : 'invalid semester',
-    invYear : 'invalid year'
-};
-
-/**
- * validateSemesterField (determines if the semester in the request body matches a semester that is offered on the klass document)
- * @param {Object} reqBody  - contains the parsed request body
- * @returns {Object} Resolves with an object containing the status code, an error message (if there was an error), and the klass (if successful)
- */
-function validateSemesterField(reqBody) {
-    return new Promise((resolve, reject) => {
-        Klass
-            .findOne({ _id : reqBody.class_id })
-            .then(klass => {
-                if (!klass) {
-                    /** @throws a 404 and error message if can't find associated class */
-                    resolve({ code : 404, errMsg : errMsgs.noClass, kTemplate : false });
-                } else {
-                    /**
-                     * @constant {boolean} semesterOk - if true, the semester in request body is a semester offered on the klass document
-                     * @see module.verification.klass.checkSemester
-                     */
-                    const semesterOk = klassVerifiers.checkSemester(klass.semesters_offered, reqBody.semester);
-                
-                    if (!semesterOk) {
-                        /** @throws a 400 if semester is not valid and an error message */
-                        resolve({ code : 400, errMsg : errMsgs.invSemester, kTemplate : false });
-                    } else {
-                        /** @returns a 200 if successful and the klass document */
-                        resolve({ code : 200, errMsg : null, kTemplate : klass });
-                    }
-                }
-            })
-            .catch(err => reject(err));
-    });
-}
-
-/**
- * validateYear
- * @param {Object} req - request body
- * @returns {boolean} A boolean is returned that is true if the year is valid
- */
-
-function validateYear(req) {
-    const kH = req.body;
-    if (kH.hasOwnProperty('year')) {
-        /**
-         * @constant {boolean} yearOk - this is true when the year is a valid year
-         * (NOTE: this is not defined at the level of the klassHistory model)
-         * @see module.verification.klass.checkYear
-         */
-        const yearOk = klassVerifiers.checkYear(kH.year);
-
-        if (!yearOk) {
-            /** @throws a 400 if the year is not valid */
-            req.errStatus = 400;
-            throw new Error(errMsgs.invYear);
-        }
-    }
-}
+/** @see module:verification/klassHistory */
+const klassHistoryVerifiers = require('../verification/klass-history');
 
 /**
  * resolveReqWSemester (resolves a request containing the semester field)
@@ -98,7 +29,6 @@ function validateYear(req) {
  * @callback next 
  * @returns a response to the client
  */
-
 function resolveReqWSemester(req, res, next) {
     /** 
      * @constant {Object} kH - contains the parsed request body 
@@ -110,7 +40,8 @@ function resolveReqWSemester(req, res, next) {
     /** @var {Object} kTemplate - contains the klass document found in validateSemesterField... prevents having to touch another collection again for info we already have on hand */
     let kTemplate;
 
-    validateSemesterField(kH)
+    /** @see module:verification/klassHistory.validateSemesterField */
+    klassHistoryVerifiers.validateSemesterField(kH)
         .then(semesterCheck => {
             if (semesterCheck.code !== 200) {
                 /** @throws whatever error results from the semester check (a 400) */
@@ -140,12 +71,13 @@ function resolveReqWSemester(req, res, next) {
 
             (method === 'POST') ? status = 201 : status = 200;
 
-            /** @see module:models.klassHistory.method.showKlass */
+            /** @see module:models/klassHistory.method#showKlass */
             return res.status(status).json({ class_history : newKH.showKlass(kTemplate) });
         })
         .catch(next);
 }
 
+/** @exports klassHistoryController */
 module.exports = {
     /**
      * Creates a new KlassHistory document
@@ -175,8 +107,10 @@ module.exports = {
             throw new Error(reqStatus.msg);
         }
 
-        validateYear(req);
+        /** @see module:klassHistoryVerifiers.validateYear */
+        klassHistoryVerifiers.validateYear(req);
 
+        /** @function resolveReqWSemester */
         resolveReqWSemester(req, res, next);
     },
     /**
@@ -212,7 +146,7 @@ module.exports = {
             .find()
             .populate('class_id')
             .then(kHs => {
-                /** @see module.models.klassHistory.method.showKlass */
+                /** @see module:models/klassHistory.method#showKlass */
                 return res.status(200).json({ class_histories : kHs.map(kH => kH.showKlass(kH.class_id)) });
             })
             .catch(next);
@@ -235,7 +169,7 @@ module.exports = {
                     throw new Error('cannot find classHistory ', req.params.id);
                 }
 
-                /** @see module.models.klassHistory.method.showKlass */
+                /** @see module:models/klassHistory.method#showKlass */
                 return res.status(200).json({ class_history : kH.showKlass(kH.class_id) });
             })
             .catch(next);
@@ -289,6 +223,7 @@ module.exports = {
          * @var {Object} reqStatus - determines the status of the request (whether ok or not)
          * @property {boolean} req.Status.isOk - if false the request body is badly-formed
          * @property {string} req.Status.msg - if exists, it provides a message to send to the client
+         * @see module:checkers.checkReqFields
          */
         let reqStatus = checkReqFields(reqFields, uKH);
 
@@ -298,8 +233,8 @@ module.exports = {
             throw new Error(reqStatus.msg);
         }
 
-        /** @function validateYear */
-        validateYear(req);
+        /** @see module:klassHistoryVerifiers.validateYear */
+        klassHistoryVerifiers.validateYear(req);
 
         if (uKH.hasOwnProperty('semester')) {
             /** @function resolveReqWSemester */
